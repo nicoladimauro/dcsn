f89fc98ab184319fde7981d63e00663d66d0ee91
Nicola Di Mauro <nicola.dimauro@uniba.it>
2016-02-23 15:15:14 +0100

    removing big endian

--- preprocess.sh
+++ preprocess.sh
@@ -1,19 +1,19 @@
-python3 preprocessing.py Arts1500 -c 26 -b -k 5
-python3 preprocessing.py birds -c 19 -b -k 5
-python3 preprocessing.py Business1500 -c 30 -b -k 5
-python3 preprocessing.py CAL500 -c 174 -b -k 5
-python3 preprocessing.py emotions -c 6 -b -k 5
-python3 preprocessing.py flags -c 7 -b -k 5
-python3 preprocessing.py Health1500 -c 32 -b -k 5
-python3 preprocessing.py human3106 -c 14 -b -k 5
-python3 preprocessing.py plant978 -c 12 -b -k 5
-python3 preprocessing.py rcv1subset1500 -c 101 -b -k 5
-python3 preprocessing.py rcv1subset2500 -c 101 -b -k 5
-python3 preprocessing.py rcv1subset3500 -c 101 -b -k 5
-python3 preprocessing.py rcv1subset4500 -c 101 -b -k 5
-python3 preprocessing.py rcv1subset5500 -c 101 -b -k 5
-python3 preprocessing.py rcv1subset5500 -c 101 -b -k 5
-python3 preprocessing.py scene -c 6 -b -k 5
-python3 preprocessing.py yeast -c 14 -b -k 5
+python3 preprocessing.py Arts1500 -c 26 -k 5
+python3 preprocessing.py birds -c 19 -k 5
+python3 preprocessing.py Business1500 -c 30 -k 5
+python3 preprocessing.py CAL500 -c 174 -k 5
+python3 preprocessing.py emotions -c 6 -k 5
+python3 preprocessing.py flags -c 7 -k 5
+python3 preprocessing.py Health1500 -c 32 -k 5
+python3 preprocessing.py human3106 -c 14 -k 5
+python3 preprocessing.py plant978 -c 12 -k 5
+python3 preprocessing.py rcv1subset1500 -c 101 -k 5
+python3 preprocessing.py rcv1subset2500 -c 101 -k 5
+python3 preprocessing.py rcv1subset3500 -c 101 -k 5
+python3 preprocessing.py rcv1subset4500 -c 101 -k 5
+python3 preprocessing.py rcv1subset5500 -c 101 -k 5
+python3 preprocessing.py rcv1subset5500 -c 101 -k 5
+python3 preprocessing.py scene -c 6 -k 5
+python3 preprocessing.py yeast -c 14 -k 5
 
 
