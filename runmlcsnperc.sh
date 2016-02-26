41d60e6b58d7303482d2e7fc5529a17cb831afed
Nicola Di Mauro <nicola.dimauro@uniba.it>
2016-02-26 10:38:25 +0100

    a new script

---
+++ runmlcsnperc.sh
@@ -0,0 +1,20 @@
+python3 runmlcsn.py emotions -c 6 -l -d 0.4 -o ./exp/csn/p40/ > ./exp/csn/p40/emotions.log
+python3 runmlcsn.py Arts1500 -c 26 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/arts.log
+python3 runmlcsn.py birds -c 19 -l -d 0.4 -o ./exp/csn/p40/ > ./exp/csn/p40/birds.log
+python3 runmlcsn.py Business1500 -c 30 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/business.log
+python3 runmlcsn.py CAL500 -c 174 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/cal.log
+python3 runmlcsn.py emotions -c 6 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/emotions.log
+python3 runmlcsn.py flags -c 7 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/flags.log
+python3 runmlcsn.py Health1500 -c 32 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/health.log
+python3 runmlcsn.py human3106 -c 14 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/human.log
+python3 runmlcsn.py plant978 -c 12 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/plant.log
+python3 runmlcsn.py rcv1subset1500 -c 101 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/rcv15.log
+python3 runmlcsn.py rcv1subset1500 -c 101 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/rcv15.log
+python3 runmlcsn.py rcv1subset2500 -c 101 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/rcv25.log
+python3 runmlcsn.py rcv1subset3500 -c 101 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/rcv35.log
+python3 runmlcsn.py rcv1subset4500 -c 101 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/rcv45.log
+python3 runmlcsn.py rcv1subset5500 -c 101  -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/rcv55.log
+python3 runmlcsn.py scene -c 6 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/scene.log
+python3 runmlcsn.py yeast -c 14 -l -d 0.4  -o ./exp/csn/p40/ > ./exp/csn/p40/yeast.log
+
+
