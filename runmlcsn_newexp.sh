e9b615d360b02ba7ec9983996b9dcad32521f0d4
Nicola Di Mauro <nicola.dimauro@uniba.it>
2016-04-19 10:43:55 +0200

    script for nips exp

---
+++ runmlcsn_newexp.sh
@@ -0,0 +1,208 @@
+mkdir exp/newcsn/l1.0ts0l/
+python3 runmlcsn.py Arts1500 -c 26 -l -d 1 -o ./exp/newcsn/l1.0ts0l/ > ./exp/newcsn/l1.0ts0l/arts.log
+python3 runmlcsn.py birds -c 19 -l -d 1 -o ./exp/newcsn/l1.0ts0l/ > ./exp/newcsn/l1.0ts0l/birds.log
+python3 runmlcsn.py Business1500 -c 30 -l -d 1  -o ./exp/newcsn/l1.0ts0l/ > ./exp/newcsn/l1.0ts0l/business.log
+python3 runmlcsn.py CAL500 -c 174 -l -d 1  -o ./exp/newcsn/l1.0ts0l/ > ./exp/newcsn/l1.0ts0l/cal.log
+python3 runmlcsn.py emotions -c 6 -l -d 1  -o ./exp/newcsn/l1.0ts0l/ > ./exp/newcsn/l1.0ts0l/emotions.log
+python3 runmlcsn.py flags -c 7 -l -d 1  -o ./exp/newcsn/l1.0ts0l/ > ./exp/newcsn/l1.0ts0l/flags.log
+python3 runmlcsn.py Health1500 -c 32 -l -d 1  -o ./exp/newcsn/l1.0ts0l/ > ./exp/newcsn/l1.0ts0l/health.log
+python3 runmlcsn.py human3106 -c 14 -l -d 1  -o ./exp/newcsn/l1.0ts0l/ > ./exp/newcsn/l1.0ts0l/human.log
+python3 runmlcsn.py plant978 -c 12 -l -d 1  -o ./exp/newcsn/l1.0ts0l/ > ./exp/newcsn/l1.0ts0l/plant.log
+python3 runmlcsn.py scene -c 6 -l -d 1  -o ./exp/newcsn/l1.0ts0l/ > ./exp/newcsn/l1.0ts0l/scene.log
+python3 runmlcsn.py yeast -c 14 -l -d 1  -o ./exp/newcsn/l1.0ts0l/ > ./exp/newcsn/l1.0ts0l/yeast.log
+
+mkdir exp/newcsn/l1.0ts1l/
+python3 runmlcsn.py Arts1l500 -c 26 -l -d 1 -ts 1 -o ./exp/newcsn/l1.0ts1l/ > ./exp/newcsn/l1.0ts1l/arts.log
+python3 runmlcsn.py birds -c 19 -l -d 1 -ts 1 -o ./exp/newcsn/l1.0ts1l/ > ./exp/newcsn/l1.0ts1l/birds.log
+python3 runmlcsn.py Business1500 -c 30 -l -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1l/ > ./exp/newcsn/l1.0ts1l/business.log
+python3 runmlcsn.py CAL500 -c 174 -l -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1l/ > ./exp/newcsn/l1.0ts1l/cal.log
+python3 runmlcsn.py emotions -c 6 -l -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1l/ > ./exp/newcsn/l1.0ts1l/emotions.log
+python3 runmlcsn.py flags -c 7 -l -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1l/ > ./exp/newcsn/l1.0ts1l/flags.log
+python3 runmlcsn.py Health1500 -c 32 -l -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1l/ > ./exp/newcsn/l1.0ts1l/health.log
+python3 runmlcsn.py human3106 -c 14 -l -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1l/ > ./exp/newcsn/l1.0ts1l/human.log
+python3 runmlcsn.py plant978 -c 12 -l -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1l/ > ./exp/newcsn/l1.0ts1l/plant.log
+python3 runmlcsn.py scene -c 6 -l -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1l/ > ./exp/newcsn/l1.0ts1l/scene.log
+python3 runmlcsn.py yeast -c 14 -l -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1l/ > ./exp/newcsn/l1.0ts1l/yeast.log
+
+mkdir exp/newcsn/l1.0ts2l/
+python3 runmlcsn.py Arts1500 -c 26 -l -d 1 -ts 2 -o ./exp/newcsn/l1.0ts2l/ > ./exp/newcsn/l1.0ts2l/arts.log
+python3 runmlcsn.py birds -c 19 -l -d 1 -ts 2 -o ./exp/newcsn/l1.0ts2l/ > ./exp/newcsn/l1.0ts2l/birds.log
+python3 runmlcsn.py Business1500 -c 30 -l -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2l/ > ./exp/newcsn/l1.0ts2l/business.log
+python3 runmlcsn.py CAL500 -c 174 -l -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2l/ > ./exp/newcsn/l1.0ts2l/cal.log
+python3 runmlcsn.py emotions -c 6 -l -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2l/ > ./exp/newcsn/l1.0ts2l/emotions.log
+python3 runmlcsn.py flags -c 7 -l -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2l/ > ./exp/newcsn/l1.0ts2l/flags.log
+python3 runmlcsn.py Health1500 -c 32 -l -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2l/ > ./exp/newcsn/l1.0ts2l/health.log
+python3 runmlcsn.py human3106 -c 14 -l -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2l/ > ./exp/newcsn/l1.0ts2l/human.log
+python3 runmlcsn.py plant978 -c 12 -l -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2l/ > ./exp/newcsn/l1.0ts2l/plant.log
+python3 runmlcsn.py scene -c 6 -l -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2l/ > ./exp/newcsn/l1.0ts2l/scene.log
+python3 runmlcsn.py yeast -c 14 -l -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2l/ > ./exp/newcsn/l1.0ts2l/yeast.log
+
+mkdir exp/newcsn/l1.0ts3l/
+python3 runmlcsn.py Arts1500 -c 26 -l -d 1 -ts 3 -o ./exp/newcsn/l1.0ts3l/ > ./exp/newcsn/l1.0ts3l/arts.log
+python3 runmlcsn.py birds -c 19 -l -d 1 -ts 3 -o ./exp/newcsn/l1.0ts3l/ > ./exp/newcsn/l1.0ts3l/birds.log
+python3 runmlcsn.py Business1500 -c 30 -l -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3l/ > ./exp/newcsn/l1.0ts3l/business.log
+python3 runmlcsn.py CAL500 -c 174 -l -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3l/ > ./exp/newcsn/l1.0ts3l/cal.log
+python3 runmlcsn.py emotions -c 6 -l -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3l/ > ./exp/newcsn/l1.0ts3l/emotions.log
+python3 runmlcsn.py flags -c 7 -l -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3l/ > ./exp/newcsn/l1.0ts3l/flags.log
+python3 runmlcsn.py Health1500 -c 32 -l -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3l/ > ./exp/newcsn/l1.0ts3l/health.log
+python3 runmlcsn.py human3106 -c 14 -l -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3l/ > ./exp/newcsn/l1.0ts3l/human.log
+python3 runmlcsn.py plant978 -c 12 -l -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3l/ > ./exp/newcsn/l1.0ts3l/plant.log
+python3 runmlcsn.py scene -c 6 -l -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3l/ > ./exp/newcsn/l1.0ts3l/scene.log
+python3 runmlcsn.py yeast -c 14 -l -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3l/ > ./exp/newcsn/l1.0ts3l/yeast.log
+
+mkdir exp/newcsn/l1.0ts0/
+python3 runmlcsn.py Arts1500 -c 26 -d 1 -o ./exp/newcsn/l1.0ts0/ > ./exp/newcsn/l1.0ts0/arts.log
+python3 runmlcsn.py birds -c 19 -d 1 -o ./exp/newcsn/l1.0ts0/ > ./exp/newcsn/l1.0ts0/birds.log
+python3 runmlcsn.py Business1500 -c 30 -d 1  -o ./exp/newcsn/l1.0ts0/ > ./exp/newcsn/l1.0ts0/business.log
+python3 runmlcsn.py CAL500 -c 174 -d 1  -o ./exp/newcsn/l1.0ts0/ > ./exp/newcsn/l1.0ts0/cal.log
+python3 runmlcsn.py emotions -c 6 -d 1  -o ./exp/newcsn/l1.0ts0/ > ./exp/newcsn/l1.0ts0/emotions.log
+python3 runmlcsn.py flags -c 7 -d 1  -o ./exp/newcsn/l1.0ts0/ > ./exp/newcsn/l1.0ts0/flags.log
+python3 runmlcsn.py Health1500 -c 32 -d 1  -o ./exp/newcsn/l1.0ts0/ > ./exp/newcsn/l1.0ts0/health.log
+python3 runmlcsn.py human3106 -c 14 -d 1  -o ./exp/newcsn/l1.0ts0/ > ./exp/newcsn/l1.0ts0/human.log
+python3 runmlcsn.py plant978 -c 12 -d 1  -o ./exp/newcsn/l1.0ts0/ > ./exp/newcsn/l1.0ts0/plant.log
+python3 runmlcsn.py scene -c 6 -d 1  -o ./exp/newcsn/l1.0ts0/ > ./exp/newcsn/l1.0ts0/scene.log
+python3 runmlcsn.py yeast -c 14 -d 1  -o ./exp/newcsn/l1.0ts0/ > ./exp/newcsn/l1.0ts0/yeast.log
+
+mkdir exp/newcsn/l1.0ts1/
+python3 runmlcsn.py Arts1500 -c 26 -d 1 -ts 1 -o ./exp/newcsn/l1.0ts1/ > ./exp/newcsn/l1.0ts1/arts.log
+python3 runmlcsn.py birds -c 19 -d 1 -ts 1 -o ./exp/newcsn/l1.0ts1/ > ./exp/newcsn/l1.0ts1/birds.log
+python3 runmlcsn.py Business1500 -c 30 -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1/ > ./exp/newcsn/l1.0ts1/business.log
+python3 runmlcsn.py CAL500 -c 174 -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1/ > ./exp/newcsn/l1.0ts1/cal.log
+python3 runmlcsn.py emotions -c 6 -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1/ > ./exp/newcsn/l1.0ts1/emotions.log
+python3 runmlcsn.py flags -c 7 -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1/ > ./exp/newcsn/l1.0ts1/flags.log
+python3 runmlcsn.py Health1500 -c 32 -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1/ > ./exp/newcsn/l1.0ts1/health.log
+python3 runmlcsn.py human3106 -c 14 -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1/ > ./exp/newcsn/l1.0ts1/human.log
+python3 runmlcsn.py plant978 -c 12 -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1/ > ./exp/newcsn/l1.0ts1/plant.log
+python3 runmlcsn.py scene -c 6 -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1/ > ./exp/newcsn/l1.0ts1/scene.log
+python3 runmlcsn.py yeast -c 14 -d 1 -ts 1  -o ./exp/newcsn/l1.0ts1/ > ./exp/newcsn/l1.0ts1/yeast.log
+
+mkdir exp/newcsn/l1.0ts2/
+python3 runmlcsn.py Arts1500 -c 26 -d 1 -ts 2 -o ./exp/newcsn/l1.0ts2/ > ./exp/newcsn/l1.0ts2/arts.log
+python3 runmlcsn.py birds -c 19 -d 1 -ts 2 -o ./exp/newcsn/l1.0ts2/ > ./exp/newcsn/l1.0ts2/birds.log
+python3 runmlcsn.py Business1500 -c 30 -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2/ > ./exp/newcsn/l1.0ts2/business.log
+python3 runmlcsn.py CAL500 -c 174 -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2/ > ./exp/newcsn/l1.0ts2/cal.log
+python3 runmlcsn.py emotions -c 6 -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2/ > ./exp/newcsn/l1.0ts2/emotions.log
+python3 runmlcsn.py flags -c 7 -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2/ > ./exp/newcsn/l1.0ts2/flags.log
+python3 runmlcsn.py Health1500 -c 32 -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2/ > ./exp/newcsn/l1.0ts2/health.log
+python3 runmlcsn.py human3106 -c 14 -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2/ > ./exp/newcsn/l1.0ts2/human.log
+python3 runmlcsn.py plant978 -c 12 -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2/ > ./exp/newcsn/l1.0ts2/plant.log
+python3 runmlcsn.py scene -c 6 -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2/ > ./exp/newcsn/l1.0ts2/scene.log
+python3 runmlcsn.py yeast -c 14 -d 1 -ts 2  -o ./exp/newcsn/l1.0ts2/ > ./exp/newcsn/l1.0ts2/yeast.log
+
+mkdir exp/newcsn/l1.0ts3/
+python3 runmlcsn.py Arts1500 -c 26 -d 1 -ts 3 -o ./exp/newcsn/l1.0ts3/ > ./exp/newcsn/l1.0ts3/arts.log
+python3 runmlcsn.py birds -c 19 -d 1 -ts 3 -o ./exp/newcsn/l1.0ts3/ > ./exp/newcsn/l1.0ts3/birds.log
+python3 runmlcsn.py Business1500 -c 30 -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3/ > ./exp/newcsn/l1.0ts3/business.log
+python3 runmlcsn.py CAL500 -c 174 -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3/ > ./exp/newcsn/l1.0ts3/cal.log
+python3 runmlcsn.py emotions -c 6 -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3/ > ./exp/newcsn/l1.0ts3/emotions.log
+python3 runmlcsn.py flags -c 7 -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3/ > ./exp/newcsn/l1.0ts3/flags.log
+python3 runmlcsn.py Health1500 -c 32 -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3/ > ./exp/newcsn/l1.0ts3/health.log
+python3 runmlcsn.py human3106 -c 14 -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3/ > ./exp/newcsn/l1.0ts3/human.log
+python3 runmlcsn.py plant978 -c 12 -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3/ > ./exp/newcsn/l1.0ts3/plant.log
+python3 runmlcsn.py scene -c 6 -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3/ > ./exp/newcsn/l1.0ts3/scene.log
+python3 runmlcsn.py yeast -c 14 -d 1 -ts 3  -o ./exp/newcsn/l1.0ts3/ > ./exp/newcsn/l1.0ts3/yeast.log
+
+mkdir exp/newcsn/l0.6ts0l/
+python3 runmlcsn.py Arts1500 -c 26 -l -d 0.6 -o ./exp/newcsn/l0.6ts0l/ > ./exp/newcsn/l0.6ts0l/arts.log
+python3 runmlcsn.py birds -c 19 -l -d 0.6 -o ./exp/newcsn/l0.6ts0l/ > ./exp/newcsn/l0.6ts0l/birds.log
+python3 runmlcsn.py Business1500 -c 30 -l -d 0.6  -o ./exp/newcsn/l0.6ts0l/ > ./exp/newcsn/l0.6ts0l/business.log
+python3 runmlcsn.py CAL500 -c 174 -l -d 0.6  -o ./exp/newcsn/l0.6ts0l/ > ./exp/newcsn/l0.6ts0l/cal.log
+python3 runmlcsn.py emotions -c 6 -l -d 0.6  -o ./exp/newcsn/l0.6ts0l/ > ./exp/newcsn/l0.6ts0l/emotions.log
+python3 runmlcsn.py flags -c 7 -l -d 0.6  -o ./exp/newcsn/l0.6ts0l/ > ./exp/newcsn/l0.6ts0l/flags.log
+python3 runmlcsn.py Health1500 -c 32 -l -d 0.6  -o ./exp/newcsn/l0.6ts0l/ > ./exp/newcsn/l0.6ts0l/health.log
+python3 runmlcsn.py human3106 -c 14 -l -d 0.6  -o ./exp/newcsn/l0.6ts0l/ > ./exp/newcsn/l0.6ts0l/human.log
+python3 runmlcsn.py plant978 -c 12 -l -d 0.6  -o ./exp/newcsn/l0.6ts0l/ > ./exp/newcsn/l0.6ts0l/plant.log
+python3 runmlcsn.py scene -c 6 -l -d 0.6  -o ./exp/newcsn/l0.6ts0l/ > ./exp/newcsn/l0.6ts0l/scene.log
+python3 runmlcsn.py yeast -c 14 -l -d 0.6  -o ./exp/newcsn/l0.6ts0l/ > ./exp/newcsn/l0.6ts0l/yeast.log
+
+mkdir exp/newcsn/l0.6ts1l/
+python3 runmlcsn.py Arts1l500 -c 26 -l -d 0.6 -ts 1 -o ./exp/newcsn/l0.6ts1l/ > ./exp/newcsn/l0.6ts1l/arts.log
+python3 runmlcsn.py birds -c 19 -l -d 0.6 -ts 1 -o ./exp/newcsn/l0.6ts1l/ > ./exp/newcsn/l0.6ts1l/birds.log
+python3 runmlcsn.py Business1500 -c 30 -l -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1l/ > ./exp/newcsn/l0.6ts1l/business.log
+python3 runmlcsn.py CAL500 -c 174 -l -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1l/ > ./exp/newcsn/l0.6ts1l/cal.log
+python3 runmlcsn.py emotions -c 6 -l -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1l/ > ./exp/newcsn/l0.6ts1l/emotions.log
+python3 runmlcsn.py flags -c 7 -l -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1l/ > ./exp/newcsn/l0.6ts1l/flags.log
+python3 runmlcsn.py Health1500 -c 32 -l -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1l/ > ./exp/newcsn/l0.6ts1l/health.log
+python3 runmlcsn.py human3106 -c 14 -l -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1l/ > ./exp/newcsn/l0.6ts1l/human.log
+python3 runmlcsn.py plant978 -c 12 -l -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1l/ > ./exp/newcsn/l0.6ts1l/plant.log
+python3 runmlcsn.py scene -c 6 -l -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1l/ > ./exp/newcsn/l0.6ts1l/scene.log
+python3 runmlcsn.py yeast -c 14 -l -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1l/ > ./exp/newcsn/l0.6ts1l/yeast.log
+
+mkdir exp/newcsn/l0.6ts2l/
+python3 runmlcsn.py Arts1500 -c 26 -l -d 0.6 -ts 2 -o ./exp/newcsn/l0.6ts2l/ > ./exp/newcsn/l0.6ts2l/arts.log
+python3 runmlcsn.py birds -c 19 -l -d 0.6 -ts 2 -o ./exp/newcsn/l0.6ts2l/ > ./exp/newcsn/l0.6ts2l/birds.log
+python3 runmlcsn.py Business1500 -c 30 -l -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2l/ > ./exp/newcsn/l0.6ts2l/business.log
+python3 runmlcsn.py CAL500 -c 174 -l -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2l/ > ./exp/newcsn/l0.6ts2l/cal.log
+python3 runmlcsn.py emotions -c 6 -l -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2l/ > ./exp/newcsn/l0.6ts2l/emotions.log
+python3 runmlcsn.py flags -c 7 -l -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2l/ > ./exp/newcsn/l0.6ts2l/flags.log
+python3 runmlcsn.py Health1500 -c 32 -l -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2l/ > ./exp/newcsn/l0.6ts2l/health.log
+python3 runmlcsn.py human3106 -c 14 -l -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2l/ > ./exp/newcsn/l0.6ts2l/human.log
+python3 runmlcsn.py plant978 -c 12 -l -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2l/ > ./exp/newcsn/l0.6ts2l/plant.log
+python3 runmlcsn.py scene -c 6 -l -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2l/ > ./exp/newcsn/l0.6ts2l/scene.log
+python3 runmlcsn.py yeast -c 14 -l -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2l/ > ./exp/newcsn/l0.6ts2l/yeast.log
+
+mkdir exp/newcsn/l0.6ts3l/
+python3 runmlcsn.py Arts1500 -c 26 -l -d 0.6 -ts 3 -o ./exp/newcsn/l0.6ts3l/ > ./exp/newcsn/l0.6ts3l/arts.log
+python3 runmlcsn.py birds -c 19 -l -d 0.6 -ts 3 -o ./exp/newcsn/l0.6ts3l/ > ./exp/newcsn/l0.6ts3l/birds.log
+python3 runmlcsn.py Business1500 -c 30 -l -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3l/ > ./exp/newcsn/l0.6ts3l/business.log
+python3 runmlcsn.py CAL500 -c 174 -l -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3l/ > ./exp/newcsn/l0.6ts3l/cal.log
+python3 runmlcsn.py emotions -c 6 -l -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3l/ > ./exp/newcsn/l0.6ts3l/emotions.log
+python3 runmlcsn.py flags -c 7 -l -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3l/ > ./exp/newcsn/l0.6ts3l/flags.log
+python3 runmlcsn.py Health1500 -c 32 -l -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3l/ > ./exp/newcsn/l0.6ts3l/health.log
+python3 runmlcsn.py human3106 -c 14 -l -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3l/ > ./exp/newcsn/l0.6ts3l/human.log
+python3 runmlcsn.py plant978 -c 12 -l -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3l/ > ./exp/newcsn/l0.6ts3l/plant.log
+python3 runmlcsn.py scene -c 6 -l -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3l/ > ./exp/newcsn/l0.6ts3l/scene.log
+python3 runmlcsn.py yeast -c 14 -l -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3l/ > ./exp/newcsn/l0.6ts3l/yeast.log
+
+mkdir exp/newcsn/l0.6ts0/
+python3 runmlcsn.py Arts1500 -c 26 -d 0.6 -o ./exp/newcsn/l0.6ts0/ > ./exp/newcsn/l0.6ts0/arts.log
+python3 runmlcsn.py birds -c 19 -d 0.6 -o ./exp/newcsn/l0.6ts0/ > ./exp/newcsn/l0.6ts0/birds.log
+python3 runmlcsn.py Business1500 -c 30 -d 0.6  -o ./exp/newcsn/l0.6ts0/ > ./exp/newcsn/l0.6ts0/business.log
+python3 runmlcsn.py CAL500 -c 174 -d 0.6  -o ./exp/newcsn/l0.6ts0/ > ./exp/newcsn/l0.6ts0/cal.log
+python3 runmlcsn.py emotions -c 6 -d 0.6  -o ./exp/newcsn/l0.6ts0/ > ./exp/newcsn/l0.6ts0/emotions.log
+python3 runmlcsn.py flags -c 7 -d 0.6  -o ./exp/newcsn/l0.6ts0/ > ./exp/newcsn/l0.6ts0/flags.log
+python3 runmlcsn.py Health1500 -c 32 -d 0.6  -o ./exp/newcsn/l0.6ts0/ > ./exp/newcsn/l0.6ts0/health.log
+python3 runmlcsn.py human3106 -c 14 -d 0.6  -o ./exp/newcsn/l0.6ts0/ > ./exp/newcsn/l0.6ts0/human.log
+python3 runmlcsn.py plant978 -c 12 -d 0.6  -o ./exp/newcsn/l0.6ts0/ > ./exp/newcsn/l0.6ts0/plant.log
+python3 runmlcsn.py scene -c 6 -d 0.6  -o ./exp/newcsn/l0.6ts0/ > ./exp/newcsn/l0.6ts0/scene.log
+python3 runmlcsn.py yeast -c 14 -d 0.6  -o ./exp/newcsn/l0.6ts0/ > ./exp/newcsn/l0.6ts0/yeast.log
+
+mkdir exp/newcsn/l0.6ts1/
+python3 runmlcsn.py Arts1500 -c 26 -d 0.6 -ts 1 -o ./exp/newcsn/l0.6ts1/ > ./exp/newcsn/l0.6ts1/arts.log
+python3 runmlcsn.py birds -c 19 -d 0.6 -ts 1 -o ./exp/newcsn/l0.6ts1/ > ./exp/newcsn/l0.6ts1/birds.log
+python3 runmlcsn.py Business1500 -c 30 -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1/ > ./exp/newcsn/l0.6ts1/business.log
+python3 runmlcsn.py CAL500 -c 174 -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1/ > ./exp/newcsn/l0.6ts1/cal.log
+python3 runmlcsn.py emotions -c 6 -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1/ > ./exp/newcsn/l0.6ts1/emotions.log
+python3 runmlcsn.py flags -c 7 -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1/ > ./exp/newcsn/l0.6ts1/flags.log
+python3 runmlcsn.py Health1500 -c 32 -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1/ > ./exp/newcsn/l0.6ts1/health.log
+python3 runmlcsn.py human3106 -c 14 -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1/ > ./exp/newcsn/l0.6ts1/human.log
+python3 runmlcsn.py plant978 -c 12 -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1/ > ./exp/newcsn/l0.6ts1/plant.log
+python3 runmlcsn.py scene -c 6 -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1/ > ./exp/newcsn/l0.6ts1/scene.log
+python3 runmlcsn.py yeast -c 14 -d 0.6 -ts 1  -o ./exp/newcsn/l0.6ts1/ > ./exp/newcsn/l0.6ts1/yeast.log
+
+mkdir exp/newcsn/l0.6ts2/
+python3 runmlcsn.py Arts1500 -c 26 -d 0.6 -ts 2 -o ./exp/newcsn/l0.6ts2/ > ./exp/newcsn/l0.6ts2/arts.log
+python3 runmlcsn.py birds -c 19 -d 0.6 -ts 2 -o ./exp/newcsn/l0.6ts2/ > ./exp/newcsn/l0.6ts2/birds.log
+python3 runmlcsn.py Business1500 -c 30 -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2/ > ./exp/newcsn/l0.6ts2/business.log
+python3 runmlcsn.py CAL500 -c 174 -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2/ > ./exp/newcsn/l0.6ts2/cal.log
+python3 runmlcsn.py emotions -c 6 -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2/ > ./exp/newcsn/l0.6ts2/emotions.log
+python3 runmlcsn.py flags -c 7 -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2/ > ./exp/newcsn/l0.6ts2/flags.log
+python3 runmlcsn.py Health1500 -c 32 -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2/ > ./exp/newcsn/l0.6ts2/health.log
+python3 runmlcsn.py human3106 -c 14 -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2/ > ./exp/newcsn/l0.6ts2/human.log
+python3 runmlcsn.py plant978 -c 12 -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2/ > ./exp/newcsn/l0.6ts2/plant.log
+python3 runmlcsn.py scene -c 6 -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2/ > ./exp/newcsn/l0.6ts2/scene.log
+python3 runmlcsn.py yeast -c 14 -d 0.6 -ts 2  -o ./exp/newcsn/l0.6ts2/ > ./exp/newcsn/l0.6ts2/yeast.log
+
+mkdir exp/newcsn/l0.6ts3/
+python3 runmlcsn.py Arts1500 -c 26 -d 0.6 -ts 3 -o ./exp/newcsn/l0.6ts3/ > ./exp/newcsn/l0.6ts3/arts.log
+python3 runmlcsn.py birds -c 19 -d 0.6 -ts 3 -o ./exp/newcsn/l0.6ts3/ > ./exp/newcsn/l0.6ts3/birds.log
+python3 runmlcsn.py Business1500 -c 30 -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3/ > ./exp/newcsn/l0.6ts3/business.log
+python3 runmlcsn.py CAL500 -c 174 -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3/ > ./exp/newcsn/l0.6ts3/cal.log
+python3 runmlcsn.py emotions -c 6 -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3/ > ./exp/newcsn/l0.6ts3/emotions.log
+python3 runmlcsn.py flags -c 7 -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3/ > ./exp/newcsn/l0.6ts3/flags.log
+python3 runmlcsn.py Health1500 -c 32 -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3/ > ./exp/newcsn/l0.6ts3/health.log
+python3 runmlcsn.py human3106 -c 14 -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3/ > ./exp/newcsn/l0.6ts3/human.log
+python3 runmlcsn.py plant978 -c 12 -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3/ > ./exp/newcsn/l0.6ts3/plant.log
+python3 runmlcsn.py scene -c 6 -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3/ > ./exp/newcsn/l0.6ts3/scene.log
+python3 runmlcsn.py yeast -c 14 -d 0.6 -ts 3  -o ./exp/newcsn/l0.6ts3/ > ./exp/newcsn/l0.6ts3/yeast.log
+
