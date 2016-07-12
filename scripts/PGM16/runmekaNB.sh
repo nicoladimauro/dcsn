5c919fc33d49571123dd4a53812f835fa450648e
Nicola Di Mauro <nicola.dimauro@uniba.it>
2016-07-12 12:34:36 +0200

    fixing scripts

---
+++ scripts/PGM16/runmekaNB.sh
@@ -0,0 +1,24 @@
+python3 runmeka.py Arts1500 -f 5 -c 26 -mc meka.classifiers.multilabel.BR -o ./exp/meka/nbbr/ > ./exp/meka/nbbr/arts.log
+python3 runmeka.py birds -c 19 -f 5 -mc meka.classifiers.multilabel.BR -o ./exp/meka/nbbr/ > ./exp/meka/nbbr/birds.log
+python3 runmeka.py Business1500 -c 30 -f 5 -mc meka.classifiers.multilabel.BR -o ./exp/meka/nbbr/ > ./exp/meka/nbbr/business.log
+python3 runmeka.py CAL500 -c 174 -f 5 -mc meka.classifiers.multilabel.BR -o ./exp/meka/nbbr/ > ./exp/meka/nbbr/cal.log
+python3 runmeka.py emotions -c 6 -f 5 -mc meka.classifiers.multilabel.BR -o ./exp/meka/nbbr/ > ./exp/meka/nbbr/emotions.log
+python3 runmeka.py flags -c 7 -f 5 -mc meka.classifiers.multilabel.BR -o ./exp/meka/nbbr/ > ./exp/meka/nbbr/flags.log
+python3 runmeka.py Health1500 -c 32 -f 5 -mc meka.classifiers.multilabel.BR -o ./exp/meka/nbbr/ > ./exp/meka/nbbr/health.log
+python3 runmeka.py human3106 -c 14 -f 5 -mc meka.classifiers.multilabel.BR -o ./exp/meka/nbbr/ > ./exp/meka/nbbr/human.log
+python3 runmeka.py plant978 -c 12 -f 5 -mc meka.classifiers.multilabel.BR -o ./exp/meka/nbbr/ > ./exp/meka/nbbr/plant.log
+python3 runmeka.py scene -c 6 -f 5 -mc meka.classifiers.multilabel.BR -o ./exp/meka/nbbr/ > ./exp/meka/nbbr/scene.log
+python3 runmeka.py yeast -c 14 -f 5 -mc meka.classifiers.multilabel.BR -o ./exp/meka/nbbr/ > ./exp/meka/nbbr/yeast.log
+
+python3 runmeka.py Arts1500 -c 26 -f 5 -mc meka.classifiers.multilabel.CC -o ./exp/meka/nbcc/ > ./exp/meka/nbcc/arts.log
+python3 runmeka.py birds -c 19 -f 5 -mc meka.classifiers.multilabel.CC -o ./exp/meka/nbcc/ > ./exp/meka/nbcc/birds.log
+python3 runmeka.py Business1500 -c 30 -f 5 -mc meka.classifiers.multilabel.CC -o ./exp/meka/nbcc/ > ./exp/meka/nbcc/business.log
+python3 runmeka.py CAL500 -c 174 -f 5 -mc meka.classifiers.multilabel.CC -o ./exp/meka/nbcc/ > ./exp/meka/nbcc/cal.log
+python3 runmeka.py emotions -c 6 -f 5 -mc meka.classifiers.multilabel.CC -o ./exp/meka/nbcc/ > ./exp/meka/nbcc/emotions.log
+python3 runmeka.py flags -c 7 -f 5 -mc meka.classifiers.multilabel.CC -o ./exp/meka/nbcc/ > ./exp/meka/nbcc/flags.log
+python3 runmeka.py Health1500 -c 32 -f 5 -mc meka.classifiers.multilabel.CC -o ./exp/meka/nbcc/ > ./exp/meka/nbcc/health.log
+python3 runmeka.py human3106 -c 14 -f 5 -mc meka.classifiers.multilabel.CC -o ./exp/meka/nbcc/ > ./exp/meka/nbcc/human.log
+python3 runmeka.py plant978 -c 12 -f 5 -mc meka.classifiers.multilabel.CC -o ./exp/meka/nbcc/ > ./exp/meka/nbcc/plant.log
+python3 runmeka.py scene -c 6 -f 5 -mc meka.classifiers.multilabel.CC -o ./exp/meka/nbcc/ > ./exp/meka/nbcc/scene.log
+python3 runmeka.py yeast -c 14 -f 5 -mc meka.classifiers.multilabel.CC -o ./exp/meka/nbcc/ > ./exp/meka/nbcc/yeast.log
+
